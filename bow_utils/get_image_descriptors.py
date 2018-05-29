f003fdace1413912d8c721f4399d253b1eeae50e
Paweł Gołąb <pawel@millionmonkeys.pl>
2018-05-29 13:11:12 +0200

    calculate-image-descriptors

--- bow_utils/get_image_descriptors.py
+++ bow_utils/get_image_descriptors.py
@@ -4,8 +4,13 @@ from config import DRAW_DETECTED_KEYPOINTS
 
 
 def get_image_descriptors(image_path, sift):
-    # ToDo: calculate image descriptors
-    # https://docs.opencv.org/3.3.0/da/df5/tutorial_py_sift_intro.html
-    descriptors = None
+    image = cv2.imread(image_path.as_posix())
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    key_points, descriptors = sift.detectAndCompute(gray_image, None)
+
+    if DRAW_DETECTED_KEYPOINTS:
+        cv2.drawKeypoints(image, key_points, image, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imshow('processing image', image)
+        cv2.waitKey(0)
 
     return descriptors
